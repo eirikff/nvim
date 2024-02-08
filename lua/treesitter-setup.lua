5f6cd548ee9ce58335cf80d879d5f336811d22ee
Eirik Falck <eirik.falck@sintef.no>
2024-02-08 13:59:30 +0100

    Update treesitter config

--- lua/treesitter-setup.lua
+++ lua/treesitter-setup.lua
@@ -18,7 +18,9 @@ vim.defer_fn(function()
     },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
+    auto_install = true,
+    sync_install = false,
+    ignore_install = {},
 
     highlight = { enable = true },
     indent = { enable = true },
