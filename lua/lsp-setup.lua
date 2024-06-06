56773db19f41c73c733d98a2a214ab95f8c8769c
Eirik Falck <Eirik.Falck@nhn.no>
2024-06-06 16:05:06 +0200

    Add pipenv recognition for pyright langserver

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -119,4 +119,25 @@ mason_lspconfig.setup_handlers {
   end,
 }
 
+
+-- Make pyright language server recognize pipenv's
+require("lspconfig").pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  on_new_config = function(new_config, root_dir)
+    local pipfile_exists = require("lspconfig").util.search_ancestors(root_dir, function(path)
+      local pipfile = require("lspconfig").util.path.join(path, "Pipfile")
+      if require("lspconfig").util.path.is_file(pipfile) then
+        return true
+      else
+        return false
+      end
+    end)
+
+    if pipfile_exists then
+      new_config.cmd = { "pipenv", "run", "pyright-langserver", "--stdio" }
+    end
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
