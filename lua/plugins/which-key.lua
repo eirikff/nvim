bad7584ab025daa236c0cc1ef3d00fca154825ce
Eirik Falck <Eirik.Falck@teledyne.com>
2025-08-29 08:32:39 +0200

    Add groups to which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,3 +1,18 @@
 return {
-  { "folke/which-key.nvim", opts = {} }
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>c", group = "Code" },
+        { "<leader>d", group = "Document" },
+        { "<leader>f", group = "Format" },
+        { "<leader>g", group = "Git" },
+        { "<leader>h", group = "Git hunk", mode = { "n", "v" } },
+        { "<leader>p", group = "Project" },
+        { "<leader>s", group = "Search" },
+        { "<leader>t", group = "Toggle" },
+        { "<leader>w", group = "Workspace" },
+      },
+    },
+  }
 }
