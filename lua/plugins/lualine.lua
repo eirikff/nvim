ba08e56ae95019197efb102a51ba00935b410856
Eirik Falck <Eirik.Falck@teledyne.com>
2025-08-29 11:00:42 +0200

    Customize lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,10 +3,24 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
-      icons_enabled = false,
-      theme = "onedark",
+      icons_enabled = true,
+      theme = "auto",
       component_separators = "|",
       section_separators = "",
+    },
+    sections = {
+      lualine_a = {"mode"},
+      lualine_b = {"branch", "diff", "diagnostics"},
+      lualine_c = {
+        {
+          "filename",
+          path = 1,
+          newfile_status = true,
+        },
+      },
+      lualine_x = {"encoding", "filetype"},
+      lualine_y = {"progress"},
+      lualine_z = {"location"}
     },
   },
 }
