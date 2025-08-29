15a3f933b3b5b61695fe7c7e2de91530b372aa89
Eirik Falck <Eirik.Falck@teledyne.com>
2025-08-29 08:59:43 +0200

    Check for version 0.11 or newer

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,3 +1,12 @@
+-- Require minimum 0.11.x
+-- We could be smarter about this, but that's more work than is worth for the 
+-- minimal value it gives
+assert(
+  vim.fn.has("nvim-0.11") == 1,
+  "This configuration requires Neovim 0.11 or newer. \z
+  Please close and install a newer version."
+)
+
 require("core.globals")
 require("core.env")
 require("core.options")
