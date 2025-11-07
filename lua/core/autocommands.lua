6171e7fd3350fc230eca5b55b10941f303ca9961
Eirik Falck <Eirik.Falck@teledyne.com>
2025-11-07 15:15:04 +0100

    Update autocommands

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,15 +1,22 @@
+-- see examples here:
+-- https://github.com/Alexis12119/nvim-config/blob/main/lua/core/autocommands.lua
+-- https://www.youtube.com/watch?v=v36vLiFVOXY
+
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
+-- highlight yanked texted
+autocmd("TextYankPost", {
+  group = augroup("YankHighlight", { clear = true }),
   pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200, })
+  end,
 })
 
--- see examples here:
--- https://github.com/Alexis12119/nvim-config/blob/main/lua/core/autocommands.lua
+-- open help in vertical split to the right
+autocmd("FileType", {
+  pattern = "help",
+  command = "wincmd L",
+})
 
