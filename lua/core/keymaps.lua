c74e8a63db5e7d1d152a3e7707be3bc6a4f81ac9
Eirik Falck <eirik.falck@teledyne.com>
2025-12-09 12:49:31 +0100

    Add keymap to navigate between diagnostics

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -48,8 +48,8 @@ vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
--- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
--- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>e", function()
   vim.diagnostic.open_float({
     close_events = { "CursorMoved", "BufLeave", "WinLeave", "LspDetach" },
