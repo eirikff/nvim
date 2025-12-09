f7382e6c652e720b697360b21bf829016520aaaa
Eirik Falck <eirik.falck@teledyne.com>
2025-12-09 11:12:21 +0100

    Update colorscheme to everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,31 @@
+-- return {
+--   "navarasu/onedark.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme("onedark")
+--   end,
+-- }
+
+-- return {
+--   "rose-pine/neovim",
+--   lazy = false,
+--   priority = 1000,
+--   name = "rose-pine",
+--   config = function()
+--     vim.cmd.colorscheme("rose-pine-moon")
+--   end
+-- }
+
 return {
-  "navarasu/onedark.nvim",
+  "sainnhe/everforest",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("onedark")
-  end,
+    vim.g.everforest_enable_italic = true
+    vim.g.everforest_background = "soft"
+    vim.g.everforest_dim_inactive_windows = true
+    vim.g.everforest_ui_contrast = "high"
+    vim.cmd.colorscheme("everforest")
+  end
 }
