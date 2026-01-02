42418668d1cc306976ddb702e9ddef7f71a9c43e
Eirik Falck <eirik.falck@teledyne.com>
2026-01-02 12:38:54 +0100

    Update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,7 +38,7 @@ return {
   -- Fuzzy Finder (files, lsp, etc)
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
+    tag = "v0.2.0",
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
